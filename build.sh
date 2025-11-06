300b2e2b9f784758d60ef9efa3993740658398bc
IsaamMJ <136082478+IsaamMJ@users.noreply.github.com>
2025-11-06 10:25:29 +0530

    Fix Netlify Flutter build - add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install Flutter
+git clone https://github.com/flutter/flutter.git ~/flutter
+export PATH="$PATH:~/flutter/bin"
+
+# Enable web support
+flutter config --enable-web
+
+# Get dependencies
+flutter pub get
+
+# Build web app
+flutter build web --flavor dev --dart-define=ENV=$ENV --dart-define=SUPABASE_URL=$SUPABASE_URL --dart-define=SUPABASE_KEY=$SUPABASE_KEY --dart-define=API_BASE_URL=$API_BASE_URL
